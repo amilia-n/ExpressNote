a281082750b136796f31292aea95db7d29164b9e
Ami Lian <amiamilian99@gmail.com>
2025-04-17 21:24:36 -0400

    card PUT route set up and tested

--- server/routes/cardRoutes.js
+++ server/routes/cardRoutes.js
@@ -5,5 +5,6 @@ const authenticateToken = require('../middleware/authMiddleware');
 
 router.post('/generate', authenticateToken, cardController.generateFlashcards);
 router.get('/:note_id', authenticateToken, cardController.getFlashcardsByNote);
+router.put('/:card_id', authenticateToken, cardController.updateFlashcard);
 
 module.exports = router;
