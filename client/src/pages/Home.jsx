f5381de028164ab8675953788ce94f742fbec4d5
Ami Lian <amiamilian99@gmail.com>
2025-04-21 16:58:20 -0400

    Home.jsx minimal setup for NoteEditor

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NoteEditor from "../components/NoteEditor";
+import "./Home.css";
+
+
+const Home = () => {
+
+
+  return (
+      <NoteEditor />
+  );
+};
+
+export default Home;
