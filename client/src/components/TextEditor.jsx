f0af35ee17c338ec4c084c1d64a926a1e34aa104
Ami Lian <amiamilian99@gmail.com>
2025-04-25 15:45:44 -0400

    tentative decison to make text editor dragable

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -14,18 +14,30 @@ export default function TextEditor({ content, onChange, position }) {
   );
 
   return (
-    <Slate
-      editor={editor}
-      value={
-        content || [
-          {
-            type: "paragraph",
-            children: [{ text: "" }],
-          },
-        ]
-      }
-      onChange={handleChange}>
-      <Editable className="editor-content" placeholder="Start typing..." />
+    <div 
+    className="editor-container"
+    style={{
+      position: 'absolute',
+      left: position?.x || 0,
+      top: position?.y || 0,
+      cursor: 'move'
+    }}
+  >
+    <Slate 
+      editor={editor} 
+      value={content || [
+        {
+          type: "paragraph",
+          children: [{ text: "" }]
+        }
+      ]}
+      onChange={handleChange}
+    >
+      <Editable 
+        className="editor-content"
+        placeholder="Start typing..."  
+      />
     </Slate>
+  </div>
   );
 }
