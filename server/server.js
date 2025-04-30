fe1ba2cb221df2f002c347e70bfcbc5760117c5d
Ami Lian <amiamilian99@gmail.com>
2025-04-30 15:47:50 -0400

    update port for render deployment issue

--- server/server.js
+++ server/server.js
@@ -26,13 +26,14 @@ app.use(express.static('public'));
 
 // CORS middleware
 // app.use(cors({
-//   origin: process.env.NODE_ENV === 'production' 
-//     ? 'https://frontend-ffqt.onrender.com'
-//     : 'http://localhost:5173',
+
 //   credentials: true, 
 // }));
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  // origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.NODE_ENV === 'production' 
+  ? 'https://frontend-ffqt.onrender.com'
+  : 'http://localhost:5173',
   credentials: true, 
 }));
 
